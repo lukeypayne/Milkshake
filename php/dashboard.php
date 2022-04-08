a07207580ac1bc06c11680fa480c925bfaab7af9
Luke Payne <lukepayne199821@gmail.com>
2022-04-08 17:35:06 +0100

    adding more

---
+++ php/dashboard.php
@@ -0,0 +1,30 @@
+<?php
+//include auth_session.php file on all user panel pages
+include("auth_session.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Dashboard - Client area</title>
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/php.css">
+</head>
+<body>
+<header class="Header">
+        <h1>Deluxe Drinks</h1>
+    </header>
+
+    <nav class="navBar" id="navBar">
+        <ul class="navClass" id="navClass">
+            <li><a aria-current="page" href="#">Home</a> | </li>
+            <li><a href="order.html">Menu</a> | </li>
+            
+            <li><a href="#">Home</a> | </li>
+            <li><a href="#">Home</a> | </li>
+            <li><a href="#">Home</a> | </li>
+        </ul>
+    </nav>
+
+</body>
+</html>
